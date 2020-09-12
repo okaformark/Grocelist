90861b61b910a0b2e40967cb37df90ee5c8ba56e
okaformark <okaformark@gmail.com>
2020-09-12 13:13:48 -0500

    initial set up complete

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const groceries = require('./routes/api/groceries');
+
+//require the db configuration file
 const connection = require('./config/db');
+
+//require .env file
 require('dotenv').config({ path: './config/.env' });
 
+// body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connection();
-const PORT = process.env.PORT || 5000;
+//use routes
+app.use('/api/groceries', groceries);
 
+// create db connection
+connection();
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
